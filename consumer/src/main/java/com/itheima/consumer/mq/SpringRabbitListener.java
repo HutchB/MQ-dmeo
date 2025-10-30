495c2f5b7afe91641e205cea5c485ebe2c988e4c
ztq <2287404995@qq.com>
2025-10-30 11:14:51 +0800

    feat(mq): 基于注解实现交换机，队列和绑定
    - 为direct.queue1和direct.queue2配置了队列绑定
    - 添加了对hmall.direct交换机的绑定，指定为DIRECT类型
    - 设置了队列的持久化属性
    - 为队列指定了路由键（red、blue、yellow）

--- consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
+++ consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
@@ -1,5 +1,9 @@
 package com.itheima.consumer.mq;
 
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -31,17 +35,26 @@ public class SpringRabbitListener {
         System.err.println("消费者1：" + message + "，时间：" + LocalTime.now());
         Thread.sleep(200);
     }
+
     @RabbitListener(queues = "fanout.queue2")
     public void listenFanoutQueue2(String message) throws InterruptedException {
         System.err.println("消费者2：" + message + "，时间：" + LocalTime.now());
         Thread.sleep(200);
     }
 
-    @RabbitListener(queues = "direct.queue1")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue1",durable = "true"),
+            exchange = @Exchange(name = "hmall.direct",type = ExchangeTypes.DIRECT),
+            key = {"red","blue"}))
     public void listenDirectQueue1(String message) throws InterruptedException {
         System.err.println("消费者1监听到direct.queue1消息 ：" + message + "，时间：" + LocalTime.now());
         Thread.sleep(200);
     }
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue2",durable = "true"),
+            exchange = @Exchange(name = "hmall.direct",type = ExchangeTypes.DIRECT),
+            key = {"red","yellow"}
+    ))
     @RabbitListener(queues = "direct.queue2")
     public void listenDirectQueue2(String message) throws InterruptedException {
         System.err.println("消费者2监听到direct.queue2消息" + message + "，时间：" + LocalTime.now());
@@ -53,15 +66,12 @@ public class SpringRabbitListener {
         System.err.println("消费者1监听到topic.queue1消息 ：" + message + "，时间：" + LocalTime.now());
         Thread.sleep(200);
     }
+
     @RabbitListener(queues = "topic.queue2")
     public void listenTopicQueue2(String message) throws InterruptedException {
         System.err.println("消费者2监听到topic.queue2消息" + message + "，时间：" + LocalTime.now());
         Thread.sleep(200);
     }
-
-
-
-
 
 
 }
